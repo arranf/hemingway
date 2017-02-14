463e353aa0a8864b8602c999e3e59199eb67e20f
Arran France <arranf@gmail.com>
2017-02-14 12:56:57 +0000

    Add external dependencies to package.json with post install script

---
+++ postinstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp -R node_modules/font-awesome/fonts ./build/fonts
+cp -R node_modules/font-awesome/scss ./build/sass
+cp -R node_modules/highlightjs/styles/github-gist.css ./build/css
+cp -R node_modules/highlightjs/highlight.pack.js ./build/js
+cp -R node_modules/umbrellajs/umbrella.js ./build/js
